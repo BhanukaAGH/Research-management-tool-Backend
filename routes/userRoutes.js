485f7c4318348f653a140a1bdab3a91952706281
Rivindu2 <89679753+Rivindu2@users.noreply.github.com>
2022-05-02 17:59:32 +0530

    update user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,7 +15,7 @@ const { list, find1,Update,Delete,DeleteM,findby } = require('../controllers/Man
 
 router.route('/list').get(list)
 router.route('/find1/:id').get(find1)
-router.route('/update1/:id').patch(Update)
+router.route('/update1/:id').post(Update)
 router.route('/delete1/:id').delete(Delete)
 router.route('/deletem/:ids').delete(DeleteM)
 router.route('/findby/:role').get(findby)
