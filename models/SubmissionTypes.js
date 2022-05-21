44fb78de122e3be50c6eba288dd11d922f8dba8c
BhanukaAGH <it20009472@my.sliit.lk>
2022-05-21 23:38:12 +0530

    add new attribute to submitType model

--- models/SubmissionTypes.js
+++ models/SubmissionTypes.js
@@ -1,31 +1,38 @@
 const mongoose = require('mongoose')
 
-const SubTypeSchema = new mongoose.Schema({
+const SubTypeSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Please provide name'],
-        minlength: 3,
-        maxlength: 50,
-      },
-    dueDate: {
-        type: String,
-        required: [true, 'Please provide Date']
-      },
-      type: {
-        type: String,
-        required: [true, 'Please provide Type'],
-        minlength: 3,
-        maxlength: 50,
-      },
-      description: {
-        type: String,
-        minlength: 3,
-        maxlength: 50,
-      },
-      
+      type: String,
+      required: [true, 'Please provide name'],
+      minlength: 3,
+      maxlength: 50,
     },
-    { timestamps: true }
-  );
-  
+    dueDate: {
+      type: String,
+      required: [true, 'Please provide Date'],
+    },
+    type: {
+      type: String,
+      required: [true, 'Please provide Type'],
+      minlength: 3,
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+      minlength: 3,
+      maxlength: 50,
+    },
+    submitUsers: [
+      {
+        user: {
+          type: mongoose.Types.ObjectId,
+          ref: 'User',
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('SubmissionTypes', SubTypeSchema)
