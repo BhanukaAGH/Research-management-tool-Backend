bc0a620dfc6d9dfc3d18aa91dc4aae9ccd2b780a
BhanukaAGH <it20009472@my.sliit.lk>
2022-06-03 16:59:14 +0530

    update panel route

--- controllers/panelController.js
+++ controllers/panelController.js
@@ -1,155 +1,166 @@
-const Topic = require("../models/Topic");
-const StudentGroup = require("../models/StudentGroup");
-const PanelTopic = require("../models/PanelMembers");
-const User = require("../models/User");
-const Submission = require("../models/Submission");
-const sgMail = require("@sendgrid/mail");
+const Topic = require('../models/Topic')
+const StudentGroup = require('../models/StudentGroup')
+const PanelTopic = require('../models/PanelMembers')
+const User = require('../models/User')
+const Submission = require('../models/Submission')
+const sgMail = require('@sendgrid/mail')
 
 //get to accept topic by the panel member
 exports.getAcceptTopics = async (req, res) => {
   try {
     const acceptTopics = await Topic.find({
-      "supervisor.status": "approve",
-    });
+      'supervisor.status': 'approve',
+    })
 
     if (acceptTopics.length != 0) {
       return res.json({
-        status: "success",
+        status: 'success',
         numberOfResults: acceptTopics.length,
         acceptTopics,
-      });
+      })
     } else {
       return res.json({
-        status: "No any accept topic yet",
+        status: 'No any accept topic yet',
         numberOfResults: acceptTopics.length,
         acceptTopics: [],
-      });
+      })
     }
   } catch (error) {
     res.json({
-      status: "fail",
+      status: 'fail',
       result: error,
-    });
+    })
   }
-};
+}
 
 //get submission document by the student
 exports.getSubmissionDocument = async (req, res) => {
   try {
-    const checkGroup = await StudentGroup.findOne({ groupID: req.params.id });
+    const checkGroup = await StudentGroup.findOne({ groupID: req.params.id })
 
-    const { leader, member2, member3, member4 } = checkGroup;
-    const mails = [leader.email, member2.email, member3.email, member4.email];
-    const documents = await User.find({ email: { $in: mails } });
+    const { leader, member2, member3, member4 } = checkGroup
 
-    const arr = documents.map((document) => document._id);
+    const mails = [leader.email, member2.email, member3.email, member4.email]
+    const documents = await User.find({ email: { $in: mails } })
 
-    const submittedDocument = await Submission.findOne({
+    const arr = documents.map((document) => document._id)
+
+    const submittedDocument = await Submission.find({
       submitUserId: { $in: arr },
-    });
+    })
 
-    if (submittedDocument) {
+    if (!(submittedDocument.length === 0)) {
       res.json({
-        status: "success",
+        status: 'success',
         document: submittedDocument,
-      });
-      return;
+      })
+      return
     } else {
       res.json({
-        status: "This group still not submit the document",
+        status: 'This group still not submit the document',
         document: {},
-      });
+      })
     }
   } catch (error) {
     res.json({
-      status: "fail",
+      status: 'fail',
       result: error,
-    });
+    })
   }
-};
+}
 
 //get the group details
 exports.getGropudetails = async (req, res) => {
   try {
-    const groupDetails = await StudentGroup.find({ groupID: req.params.id });
+    const groupDetails = await StudentGroup.find({ groupID: req.params.id })
+
     res.json({
-      status: "success",
+      status: 'success',
       groupDetails,
-    });
+    })
   } catch (error) {
     res.json({
-      status: "fail",
+      status: 'fail',
       result: error,
-    });
+    })
   }
-};
+}
 
 //get the topics accept by panel member
 exports.getAcceptTopicsByPanelMember = async (req, res) => {
   try {
     const acceptTopics = await PanelTopic.find({
-      topicStatus: "Accept",
-    }).populate([{ path: "groupID" }, { path: "topicID" }]);
+      topicStatus: 'Accept',
+    }).populate([{ path: 'groupID' }, { path: 'topicID' }])
     res.json({
-      status: "success",
+      status: 'success',
       numberOfResults: acceptTopics.length,
       acceptTopics,
-    });
+    })
   } catch (error) {
     res.json({
-      status: "fail",
+      status: 'fail',
       result: error,
-    });
+    })
   }
-};
+}
 
 //create a accept topic by panel member
 exports.createTopicAccept = async (req, res) => {
   try {
-    const { groupID, topicStatus } = req.body;
-    let topicExits = await PanelTopic.findOne({ groupID: groupID });
+    const { groupID, topicStatus } = req.body
+    let topicExits = await PanelTopic.findOne({ groupID: groupID })
     if (topicExits) {
-      topicExits.topicStatus = topicStatus;
-      await topicExits.save();
+      topicExits.topicStatus = topicStatus
+      await topicExits.save()
     } else {
-      topicExits = await PanelTopic.create(req.body);
+      topicExits = await PanelTopic.create(req.body)
     }
     res.json({
-      status: "success",
+      status: 'success',
       topic: topicExits,
-    });
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
     res.json({
-      status: "failed",
+      status: 'failed',
       message: error,
-    });
+    })
   }
-};
+}
 
 //get the topic status
 exports.getTopicStatus = async (req, res) => {
   try {
-    const topicStatus = await PanelTopic.findOne({
+    const topicInfo = await PanelTopic.findOne({
       groupID: req.params.id,
-    });
+    })
 
-    res.json({
-      status: "success",
-      topicStatus,
-    });
+    if (topicInfo) {
+      res.json({
+        status: 'success',
+        topicInfo,
+      })
+      return
+    } else {
+      res.json({
+        status: 'success',
+        topicInfo: {},
+      })
+      return
+    }
   } catch (error) {
     res.json({
-      status: "failed",
+      status: 'failed',
       message: error,
-    });
+    })
   }
-};
+}
 
 //send feedback to the student
 exports.senEmail = async (req, res) => {
   try {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     const {
       To_whom,
       subject,
@@ -158,13 +169,13 @@ exports.senEmail = async (req, res) => {
       memberName,
       memberEmail,
       StudentName,
-    } = req.body;
+    } = req.body
     const img =
-      "https://courseweb.sliit.lk/pluginfile.php/1/theme_lambda/logo/1629135847/sliit_logo.jpg";
+      'https://courseweb.sliit.lk/pluginfile.php/1/theme_lambda/logo/1629135847/sliit_logo.jpg'
 
     const msg = {
       to: To_whom,
-      from: "hasith450@gmail.com", // Use the email address or domain you verified above
+      from: 'hasith450@gmail.com', // Use the email address or domain you verified above
       subject: subject,
       html: `
       <main style="background-color: #e0e0df;  border:2px solid black;">
@@ -180,17 +191,17 @@ exports.senEmail = async (req, res) => {
       <p>Copyright 2022 © SLIIT. All Rights Reserved. </p> 
       </main>
       `,
-    };
-    const mailResponse = await sgMail.send(msg);
+    }
+    const mailResponse = await sgMail.send(msg)
     res.json({
-      status: "success",
+      status: 'success',
       mailResponse,
-    });
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
     res.json({
-      status: "failed",
+      status: 'failed',
       error,
-    });
+    })
   }
-};
+}
--- models/PanelMembers.js
+++ models/PanelMembers.js
@@ -1,26 +1,37 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const PanelTopicSchema = new mongoose.Schema({
   topicStatus: {
     type: String,
-    default: "Pending",
-    enum: ["Pending", "Accept", "Reject"],
+    default: 'Pending',
+    enum: ['Pending', 'Accept', 'Reject'],
   },
-
   groupID: {
     type: mongoose.Types.ObjectId,
-    ref: "StudentGroup",
+    ref: 'StudentGroup',
   },
-
   topicID: {
     type: mongoose.Types.ObjectId,
-    ref: "Topic",
+    ref: 'Topic',
   },
-
+  panelMember: [
+    {
+      Name: {
+        type: String,
+        trim: true,
+        default: 'Not Allocated',
+      },
+      MemberID: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        trim: true,
+      },
+    },
+  ],
   dateAppordeOrReject: {
     type: Date,
     default: Date.now(),
   },
-});
+})
 
-module.exports = mongoose.model("PanelTopic", PanelTopicSchema);
+module.exports = mongoose.model('PanelTopic', PanelTopicSchema)
